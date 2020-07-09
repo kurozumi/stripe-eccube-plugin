7bb4fef8c0e19270f00ac76d7c4d56444fd6b5a8
kurozumi <info@a-zumi.net>
2020-07-10 03:47:04 +0900

    テンプレートのクレジットカード情報入力項目を変更

--- Form/Extension/CreditCardExtension.php
+++ Form/Extension/CreditCardExtension.php
@@ -4,29 +4,38 @@
 namespace Plugin\Stripe4\Form\Extension;
 
 
+use Eccube\Entity\Order;
 use Eccube\Form\Type\Shopping\OrderType;
+use Plugin\Stripe4\Service\Method\CreditCard;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CreditCardExtension extends AbstractTypeExtension
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        if($options['skip_add_form']) {
+        if ($options['skip_add_form']) {
             return;
         }
 
         $builder
-            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event){
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
                 $form = $event->getForm();
+                /** @var Order $order */
+                $order = $event->getData();
 
-                $form->add('stripe_token', HiddenType::class, [
-                    'required' => false,
-                    'mapped' => true
-                ]);
+                if ($order->getPayment()->getMethodClass() === CreditCard::class) {
+                    $form->add('stripe_token', HiddenType::class, [
+                        'mapped' => true,
+                        'constraints' => [
+                            new NotBlank()
+                        ]
+                    ]);
+                }
             });
     }
 
