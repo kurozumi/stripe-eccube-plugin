d6cf5b488275a420b7b41923043728eae025d57a
kurozumi <info@a-zumi.net>
2020-07-11 00:19:25 +0900

    テストの準備

---
+++ Tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+$loader = require __DIR__.'/../../../../vendor/autoload.php';
+$envFile = __DIR__.'/../../../../.env';
+if (file_exists($envFile)) {
+    (new \Symfony\Component\Dotenv\Dotenv())->load($envFile);
+}
